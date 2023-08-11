06cc1f898667fdbfb13908777f554651e19f41ce
ndndnd24 <ndndnd24@gmail.com>
2023-08-11 16:01:27 +0200

    added filter buttons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import SpendingForm from "./components/SpendingForm";
 
 function App() {
+  const [order, setOrder] = useState("");
+
   return (
     <div className="app-container">
       <div className="title-container">Spending Tracker</div>
       <SpendingForm />
+      <div className="filter-container">
+        <div className="order-filter-container">
+          <select value={order} className="sorting-container">
+            <option value="date-desc">Sort by Date descending</option>
+            <option value="date-asc">Sort by Date ascending</option>
+            <option value="amount-desc">Sort by Amount descending</option>
+            <option value="amount-asc">Sort by Amount ascending</option>
+          </select>
+        </div>
+        <div className="currency-filter-container">
+          <button className="filter-huf-button-container currency-button-container">
+            HUF
+          </button>
+          <button className="filter-usd-button-container currency-button-container">
+            USD
+          </button>
+          <button className="show-all-button-container currency-button-container active-currency-button">
+            All
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
